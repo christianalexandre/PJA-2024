15d04e4314aebbc7f53d00dc5cd9b9a179f3d093
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-05-31 16:17:04 -0300

    Correcting tasks

--- tasks/gabrielsebrao/PJA-15/temperatureConverter.kt
+++ tasks/gabrielsebrao/PJA-15/temperatureConverter.kt
@@ -1,7 +1,7 @@
 fun main() {
     printFinalTemperature(27.0, "Celsius", "Fahrenheit" , { 9.0/5.0 * 27.0 + 32.0 })
     printFinalTemperature(350.0, "Kelvin", "Celsius" , { 350.0 - 273.15 })
-    printFinalTemperature(27.0, "Celsius", "Fahrenheit" , {  5.0/9.0 * (27.0 - 32.0) + 273.15 })
+    printFinalTemperature(10.0, "Fahrenheit", "Kelvin" , { 5.0/9.0 * (10.0 - 32.0) + 273.15 })
 }
 
 fun printFinalTemperature(
--- tasks/gabrielsebrao/PJA-17/listOfInternetProfile.kt
+++ tasks/gabrielsebrao/PJA-17/listOfInternetProfile.kt
@@ -22,13 +22,14 @@ class Person(val name: String, val age: Int, val hobby: String?, val referrer: P
 }
 
 fun main() {
-    val people = listOf(
-        Person("Gabriel", 17, "play games", null),
-        Person("Daniel", 21, null, null),
-        Person("Nicolas", 30, "draw", null),
-        Person("Giulia", 18, "do the nails", null),
-        Person("Maira", 16, "design", null)
-    )
+    
+    	val gabriel = Person("Gabriel", 17, "play games", null)
+        val daniel = Person("Daniel", 21, null, gabriel)
+        val nicolas = Person("Nicolas", 30, "draw", daniel)
+        val giulia = Person("Giulia", 18, "do the nails", nicolas)
+        val maira = Person("Maira", 16, "design", null)
+    
+    val people = listOf(gabriel, daniel, nicolas, giulia, maira)
     
     people.forEach {
         it.showProfile()
