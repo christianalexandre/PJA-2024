c0501a6cc431770893a8fe93bcb84ec344821307
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-06-18 16:40:19 -0300

    style: correct lines and spaces

--- tasks/gabrielsebrao/ListaDeContatos/app/src/main/java/com/example/listadecontatos/MainActivity.kt
+++ tasks/gabrielsebrao/ListaDeContatos/app/src/main/java/com/example/listadecontatos/MainActivity.kt
@@ -2,17 +2,13 @@ package com.example.listadecontatos
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.listadecontatos.databinding.MainActivityBinding
-import java.io.Serializable
-
 
 private lateinit var binding: MainActivityBinding
 private var contactsList: MutableList<Contact> = mutableListOf()
-
 
 
 class MainActivity : ComponentActivity() {
@@ -38,15 +34,23 @@ class MainActivity : ComponentActivity() {
 
         Log.d("debug", "saveContact()")
 
-        if(name.isNotEmpty() && phone.isNotEmpty()) {
-            if(contactsList.size == 3)
-                Toast.makeText(this, "Você só pode adicionar até 3 contatos na sua lista!", Toast.LENGTH_SHORT).show()
+        if (name.isNotEmpty() && phone.isNotEmpty()) {
+            if (contactsList.size == 3)
+                Toast.makeText(
+                    this,
+                    "Você só pode adicionar até 3 contatos na sua lista!",
+                    Toast.LENGTH_SHORT
+                ).show()
             else {
                 contactsList.add(Contact(name.toString(), phone.toString()))
                 Toast.makeText(this, "Contato adicionado!", Toast.LENGTH_SHORT).show()
             }
         } else {
-            Toast.makeText(this, "Por favor, insira todos os dados do contato para salvá-lo!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "Por favor, insira todos os dados do contato para salvá-lo!",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
@@ -60,8 +64,9 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun goToContactList() {
-        if(contactsList.isEmpty())
-            Toast.makeText(this, "Por favor, adicionar pelo menos um contato!", Toast.LENGTH_SHORT).show()
+        if (contactsList.isEmpty())
+            Toast.makeText(this, "Por favor, adicionar pelo menos um contato!", Toast.LENGTH_SHORT)
+                .show()
         else {
             val intent = Intent(this, ContactListActivity::class.java).apply {
                 val bundle = Bundle().apply {
