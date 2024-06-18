6c06d5a85932a8bc36bfa11d9a6cfe35f427008e
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-06-18 16:37:56 -0300

    style: remove a space

--- tasks/gabrielsebrao/ListaDeContatos/app/src/main/java/com/example/listadecontatos/Contact.kt
+++ tasks/gabrielsebrao/ListaDeContatos/app/src/main/java/com/example/listadecontatos/Contact.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Contact (
+data class Contact(
     val name: String,
     val phone: String
 ) : Parcelable
