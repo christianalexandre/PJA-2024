d6c4f16c023469c4ee6292a06403b849a253a8d9
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-06-27 22:28:02 -0300

    feat: add a class of usefull methods

---
+++ tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/Currency.kt
@@ -0,0 +1,51 @@
+package com.example.conversaomoedas
+
+import android.widget.Spinner
+
+class Currency {
+    fun getCurrencyAbbreviation(currency: String): String {
+        when (currency) {
+            "Real (BRL)" -> {
+                return "BRL"
+            }
+
+            "Dólar Americano (USD)" -> {
+                return "USD"
+            }
+
+            "Libra Esterlina (GBP)" -> {
+                return "GBP"
+            }
+
+            "Franco Suíço (CHF)" -> {
+                return "CHF"
+            }
+
+            "Euro (EUR)" -> {
+                return "EUR"
+            }
+        }
+
+        return ""
+    }
+
+    fun defineCurrencySelectedItem(spinner: Spinner, currency: String) {
+        when(currency) {
+            "Dólar Americano (USD)" -> {
+                spinner.setSelection(1)
+            }
+            "Libra Esterlina (GBP)" -> {
+                spinner.setSelection(2)
+            }
+            "Franco Suíço (CHF)" -> {
+                spinner.setSelection(3)
+            }
+            "Real (BRL)" -> {
+                spinner.setSelection(4)
+            }
+            "Euro (EUR)" -> {
+                spinner.setSelection(5)
+            }
+        }
+    }
+}
