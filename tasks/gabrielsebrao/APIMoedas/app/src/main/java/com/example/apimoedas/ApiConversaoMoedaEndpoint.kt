b19ac40bb8ab0fbf35e7076902eafa987fb40189
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-07-09 16:54:44 -0300

    fix: remove old api features

--- tasks/gabrielsebrao/APIMoedas/app/src/main/java/com/example/apimoedas/ApiConversaoMoedaEndpoint.kt
+++
@@ -1,9 +0,0 @@
-package com.example.apimoedas
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface ApiConversaoMoedaEndpoint {
-    @GET("posts")
-    fun getPosts() : Call<List<Posts>>
-}
