6389255d56b9798b3ce955373ef09043168de2ef
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-07-11 11:21:48 -0300

    feat: add connection class

---
+++ tasks/gabrielsebrao/APIMoedas/app/src/main/java/com/example/apimoedas/Connection.kt
@@ -0,0 +1,24 @@
+package com.example.apimoedas
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+class Connection {
+    fun checkForInternet(context: Context): Boolean {
+
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val network = connectivityManager.activeNetwork ?: return false
+
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+            else -> false
+        }
+    }
+}
