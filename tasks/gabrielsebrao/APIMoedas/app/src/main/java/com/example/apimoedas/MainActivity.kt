1de3e57f5fc40c9bd443ac1d9b65d7ed380efdf2
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-07-16 15:48:13 -0300

    refactor/feat: move classes to other packages and treat no conection case

--- tasks/gabrielsebrao/APIMoedas/app/src/main/java/com/example/apimoedas/MainActivity.kt
+++ tasks/gabrielsebrao/APIMoedas/app/src/main/java/com/example/apimoedas/MainActivity.kt
@@ -2,78 +2,31 @@ package com.example.apimoedas
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
 import com.example.apimoedas.databinding.MainActivityBinding
-import retrofit2.Response
-import kotlin.text.*
-import java.net.UnknownHostException
+import com.example.apimoedas.Api.CallApi
+import com.example.apimoedas.Connection.Connection
 
 @SuppressLint("StaticFieldLeak")
-private lateinit var binding: MainActivityBinding
+lateinit var binding: MainActivityBinding
+private var connection = Connection()
+private var callApi = CallApi()
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("DefaultLocale")
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val retrofitService = RetrofitInstance.getRetrofitInstance().create(CurrencyApi::class.java)
-
-        val responseLiveData: LiveData<Response<Currency>> =
-            liveData {
-                try {
-                    val response = retrofitService.getCurrencies()
-                    emit(response)
-                } catch(_: UnknownHostException) {
-                    binding.currencyOne.text = "teste"
-                }
-            }
-
-        responseLiveData.observe(this) {
-            val currencyInfo = it.body()
-            binding.currencyOne.append("Currency name: ${currencyInfo?.USDBRL?.code} \n")
-            binding.currencyOne.append(
-                "Converted value to BRL: ${
-                    String.format(
-                        "R$%.2f",
-                        currencyInfo?.USDBRL?.bid?.toDouble()
-                    )
-                }"
-            )
-
-            binding.currencyTwo.append("Currency name: ${currencyInfo?.EURBRL?.code} \n")
-            binding.currencyTwo.append(
-                "Converted value to BRL: ${
-                    String.format(
-                        "R$%.2f",
-                        currencyInfo?.EURBRL?.bid?.toDouble()
-                    )
-                }"
-            )
-
-            binding.currencyThree.append("Currency name: ${currencyInfo?.CHFBRL?.code} \n")
-            binding.currencyThree.append(
-                "Converted value to BRL: ${
-                    String.format(
-                        "R$%.2f",
-                        currencyInfo?.CHFBRL?.bid?.toDouble()
-                    )
-                }"
-            )
-
-            binding.currencyFour.append("Currency name: ${currencyInfo?.GBPBRL?.code} \n")
-            binding.currencyFour.append(
-                "Converted value to BRL: ${
-                    String.format(
-                        "R$%.2f",
-                        currencyInfo?.GBPBRL?.bid?.toDouble()
-                    )
-                }"
-            )
+        if(!connection.checkForInternet(this)) {
+            Toast.makeText(this, "Sem internet!", Toast.LENGTH_LONG).show()
+            binding.currencyOne.text = "Conecta-se com a Internet e reinicie o app!"
+        } else {
+            callApi.callApi(this)
         }
     }
 }
