4d45a45417c717b7b66ce6a7eb33fdea99508731
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-07-16 17:13:09 -0300

    refactor: move code to methods

--- tasks/gabrielsebrao/ListaDeContatos/app/src/main/java/com/example/listadecontatos/ContactListActivity.kt
+++ tasks/gabrielsebrao/ListaDeContatos/app/src/main/java/com/example/listadecontatos/ContactListActivity.kt
@@ -16,45 +16,60 @@ class ContactListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ContactListActivityBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        val bundle = intent.getBundleExtra("bundle")
-        val contactList = bundle?.getParcelableArrayList<Contact>("contactlist")?.toList()
+        getExtras()
+        
+        setupView()
+        setupListeners()
+    }
 
-        when (contactList?.size) {
+    private fun setupView() {
+        when (contactList.size) {
             1 -> {
-                binding.contact1.visibility = View.VISIBLE
-                binding.nameContact1.text = contactList[0].name
-                binding.phoneContact1.text = contactList[0].phone
+                with(binding) {
+                    contact1.visibility = View.VISIBLE
+                    nameContact1.text = contactList[0].name
+                    phoneContact1.text = contactList[0].phone
+                }
             }
 
             2 -> {
-                binding.contact1.visibility = View.VISIBLE
-                binding.nameContact1.text = contactList[0].name
-                binding.phoneContact1.text = contactList[0].phone
+                with(binding) {
+                    contact1.visibility = View.VISIBLE
+                    nameContact1.text = contactList[0].name
+                    phoneContact1.text = contactList[0].phone
 
-                binding.contact2.visibility = View.VISIBLE
-                binding.nameContact2.text = contactList[1].name
-                binding.phoneContact2.text = contactList[1].phone
+                    contact2.visibility = View.VISIBLE
+                    nameContact2.text = contactList[1].name
+                    phoneContact2.text = contactList[1].phone
+                }
             }
 
             3 -> {
-                binding.contact1.visibility = View.VISIBLE
-                binding.nameContact1.text = contactList[0].name
-                binding.phoneContact1.text = contactList[0].phone
+                with(binding) {
+                    contact1.visibility = View.VISIBLE
+                    nameContact1.text = contactList[0].name
+                    phoneContact1.text = contactList[0].phone
 
-                binding.contact2.visibility = View.VISIBLE
-                binding.nameContact2.text = contactList[1].name
-                binding.phoneContact2.text = contactList[1].phone
+                    contact2.visibility = View.VISIBLE
+                    nameContact2.text = contactList[1].name
+                    phoneContact2.text = contactList[1].phone
 
-                binding.contact3.visibility = View.VISIBLE
-                binding.nameContact3.text = contactList[2].name
-                binding.phoneContact3.text = contactList[2].phone
+                    contact3.visibility = View.VISIBLE
+                    nameContact3.text = contactList[2].name
+                    phoneContact3.text = contactList[2].phone
+                }
             }
         }
+    }
 
+    private fun setupListeners() {
         binding.buttonReturn.setOnClickListener { goToMainActivity() }
+    }
+    private fun getExtras() {
+        val bundle = intent.getBundleExtra("bundle")
+        contactList = bundle?.getParcelableArrayList<Contact>("contactlist")?.toList()!!
     }
 
     private fun goToMainActivity() {
