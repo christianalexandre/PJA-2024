17c558b5e45d2c34e2c77ff142d84bebd86cfc6a
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-07-23 17:10:39 -0300

    refactor: change string values to a new format

--- tasks/gabrielsebrao/ListaDeContatos/app/src/main/java/com/example/listadecontatos/MainActivity.kt
+++ tasks/gabrielsebrao/ListaDeContatos/app/src/main/java/com/example/listadecontatos/MainActivity.kt
@@ -42,47 +42,33 @@ class MainActivity : ComponentActivity() {
 
         if (name.isNotEmpty() && phone.isNotEmpty()) {
             if (contactsList.size >= 3) {
-                Toast.makeText(this, R.string.exceedThreeContacts, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, R.string.exceed_three_contacts, Toast.LENGTH_SHORT).show()
             } else if (phone.length > 11) {
-                Toast.makeText(this, R.string.exceedPhoneNumber, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, R.string.exceed_phone_number, Toast.LENGTH_SHORT).show()
             } else if (name.length > 30) {
-                Toast.makeText(this, R.string.exceedName, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, R.string.exceed_name, Toast.LENGTH_SHORT).show()
             }
             else {
                 try {
                     phone.toBigInteger()
                     contactsList.add(Contact(name, phone))
-                    Toast.makeText(this, R.string.addContact, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, R.string.add_contact, Toast.LENGTH_SHORT).show()
                 } catch (_: NumberFormatException) {
-                    Toast.makeText(this, R.string.onlyNumbers, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, R.string.only_numbers, Toast.LENGTH_SHORT).show()
                 }
             }
         } else {
-            Toast.makeText(this, R.string.setAllContacts, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, R.string.set_all_data, Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun clearContacts() {
         if (contactsList.isEmpty()) {
-            Toast.makeText(this, R.string.alreadyEmptyContacts, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, R.string.already_empty_contacts, Toast.LENGTH_SHORT).show()
         } else {
             contactsList.clear()
-            Toast.makeText(this, R.string.emptyContacts, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, R.string.empty_contacts, Toast.LENGTH_SHORT).show()
         }
-    }
-
-    private fun checkPhoneValidation(phoneNumber: String): Boolean {
-        if (phoneNumber.length > 11) {
-            Toast.makeText(this, R.string.exceedPhoneNumber, Toast.LENGTH_SHORT).show()
-        } else {
-            try {
-                phoneNumber.toInt()
-            } catch (_: NumberFormatException) {
-                Toast.makeText(this, R.string.onlyNumbers, Toast.LENGTH_SHORT).show()
-                return false
-            }
-        }
-        return true
     }
 
     private fun getExtras() {
@@ -94,7 +80,7 @@ class MainActivity : ComponentActivity() {
 
     private fun goToContactList() {
         if (contactsList.isEmpty())
-            Toast.makeText(this, R.string.addAtLeastOneContact, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, R.string.add_at_least_one_contact, Toast.LENGTH_SHORT).show()
         else {
             val intent = Intent(this, ContactListActivity::class.java).apply {
                 val bundle = Bundle().apply {
