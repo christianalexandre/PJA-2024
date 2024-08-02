4be566875424037afaba81d4ab09fd10269d11e3
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-08-02 16:08:49 -0300

    style: correct spaces

--- tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/objects/Currency.kt
+++ tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/objects/Currency.kt
@@ -30,19 +30,23 @@ object Currency {
     }
 
     fun defineCurrencySelectedItem(spinner: Spinner, currency: String) {
-        when(currency) {
+        when (currency) {
             "Dólar Americano (USD)" -> {
                 spinner.setSelection(1)
             }
+
             "Libra Esterlina (GBP)" -> {
                 spinner.setSelection(2)
             }
+
             "Franco Suíço (CHF)" -> {
                 spinner.setSelection(3)
             }
+
             "Real (BRL)" -> {
                 spinner.setSelection(4)
             }
+
             "Euro (EUR)" -> {
                 spinner.setSelection(5)
             }
