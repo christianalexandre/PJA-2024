d6f6fc4c0f61da90b6a03ebb129fe5ae63c2ce98
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-08-02 16:12:28 -0300

    feat: add object to TextWatcher issues

---
+++ tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/objects/TextWatcherHelper.kt
@@ -0,0 +1,51 @@
+package com.example.conversaomoedas.objects
+
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.widget.EditText
+
+object TextWatcherHelper {
+    fun filterChangedText(text: EditText): TextWatcher {
+        return object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                val inputFilter = InputFilter { source, start, end, dest, dstart, dend ->
+                    val pattern = "^(\\d{0,8})([.,]?)$".toRegex()
+                    val decimalPattern = "^(\\d{0,8})([.,]?)(\\d{0,2})$".toRegex()
+                    val noNumberPattern = "^([.,])$".toRegex()
+                    val zeroPattern = "^(0)([^.,]*)".toRegex()
+                    val result = dest.subSequence(0, dstart).toString() +
+                            source.subSequence(start, end) +
+                            dest.subSequence(dend, dest.length).toString()
+
+                    if (noNumberPattern.matches(result)) {
+                        return@InputFilter ""
+                    }
+
+                    if (zeroPattern.matches(result) && result != "0") {
+                        return@InputFilter ""
+                    }
+
+                    if (('.' in result || ',' in result) && decimalPattern.matches(result)) {
+                        null
+                    } else if (pattern.matches(result)) {
+                        null
+                    } else {
+                        ""
+                    }
+
+                }
+
+                text.filters = arrayOf(inputFilter)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        }
+    }
+}
