a1ef9d68f936e985e85bd7291d2b99ebdba674a6
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-08-06 17:20:16 -0300

    refactor: use clean code in Conversion Page

--- tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/ConversionPage.kt
+++ tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/ConversionPage.kt
@@ -2,6 +2,8 @@ package com.example.conversaomoedas
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import com.example.conversaomoedas.objects.Currency
 import com.example.listadecontatos.R
@@ -14,126 +16,126 @@ class ConversionPage : ComponentActivity() {
     private lateinit var currencyOne: String
     private lateinit var currencyTwo: String
     private lateinit var currenciesList: Array<String>
-    private var finalValue by Delegates.notNull<Double>()
-    private var controlInitialValue by Delegates.notNull<Double>()
     private var initialValue by Delegates.notNull<Double>()
+    private var finalValue by Delegates.notNull<Double>()
+    private var convertedValue by Delegates.notNull<Double>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        initActivityVariables()
+        binding = ConversionPageBinding.inflate(layoutInflater)
+
         getExtras()
+        convertedValue = initialValue // to remember the initial value
+
+        convertValues()
         setupView()
         setupListeners()
     }
 
-    private fun initActivityVariables() {
-        binding = ConversionPageBinding.inflate(layoutInflater)
-        finalValue = 1.0
-    }
-
     private fun getExtras() {
-        val bundle = intent.getBundleExtra("bundle")
+        val bundle = intent.getBundleExtra("bundle") ?: return
 
-        currenciesList = bundle?.getStringArray("currenciesList")!!
+        currenciesList = bundle.getStringArray("currenciesList")!!
         currencyOne = Currency.getCurrencyAbbreviation(currenciesList[0])
         currencyTwo = Currency.getCurrencyAbbreviation(currenciesList[1])
 
         initialValue = bundle.getDouble("initialValue")
+    }
 
-        controlInitialValue = initialValue
+    private fun convertValues() {
+        when (currencyOne) {
+            "BRL" -> {
+                convertedValue *= 1.0
+            }
+
+            "USD" -> {
+                convertedValue *= 5.3
+            }
+
+            "GBP" -> {
+                convertedValue *= 6.74
+            }
+
+            "CHF" -> {
+                convertedValue *= 5.91
+            }
+
+            "EUR" -> {
+                convertedValue *= 5.72
+            }
+        }
+
+        when (currencyTwo) {
+            "BRL" -> {
+                finalValue = convertedValue
+            }
+
+            "USD" -> {
+                finalValue = convertedValue / 5.3
+            }
+
+            "GBP" -> {
+                finalValue = convertedValue / 6.74
+            }
+
+            "CHF" -> {
+                finalValue = convertedValue / 5.91
+            }
+
+            "EUR" -> {
+                finalValue = convertedValue / 5.72
+            }
+        }
     }
 
     private fun setupView() {
         setContentView(binding.root)
 
-        setupCurrencyOneView()
-        setupCurrencyTwoView()
+        setupCurrencyView(currencyOne, binding.flagOne, binding.initialValue, initialValue)
+        setupCurrencyView(currencyTwo, binding.flagTwo, binding.finalValue, finalValue)
+    }
 
-        finalValue = controlInitialValue / finalValue
+    private fun setupCurrencyView(currency: String, flag: ImageView, textView: TextView, value: Double) {
+        when (currency) {
+            "BRL" -> {
+                flag.setImageResource(R.drawable.brflag)
+                flag.contentDescription = "Ícone da Bandeira do Brasil"
+            }
 
-        binding.initialValue.text = String.format("%.2f $currencyOne", initialValue)
-        binding.finalValue.text = String.format("%.2f $currencyTwo", finalValue)
+            "USD" -> {
+                flag.setImageResource(R.drawable.usflag)
+                flag.contentDescription = "Ícone da bandeira dos Estados Unidos"
+            }
+
+            "GBP" -> {
+                flag.setImageResource(R.drawable.ukflag)
+                flag.contentDescription = "Ícone da bandeira do Reino Unido"
+            }
+
+            "CHF" -> {
+                flag.setImageResource(R.drawable.chflag)
+                flag.contentDescription = "Ícone da bandeira da Suíça"
+            }
+
+            "EUR" -> {
+                flag.setImageResource(R.drawable.euflag)
+                flag.contentDescription = "Ícone da bandeira da União Europeia"
+            }
+        }
+
+        textView.text = String.format("%.2f $currency", value)
     }
 
     private fun setupListeners() {
         binding.returnButton.setOnClickListener { goToMainActivity() }
     }
 
-    private fun setupCurrencyOneView() {
-        when (currencyOne) {
-            "BRL" -> {
-                controlInitialValue *= 1.0
-                binding.flagOne.setImageResource(R.drawable.brflag)
-                binding.flagOne.contentDescription = "Ícone da Bandeira do Brasil"
-            }
-
-            "USD" -> {
-                controlInitialValue *= 5.3
-                binding.flagOne.setImageResource(R.drawable.usflag)
-                binding.flagOne.contentDescription = "Ícone da bandeira dos Estados Unidos"
-            }
-
-            "GBP" -> {
-                controlInitialValue *= 6.74
-                binding.flagOne.setImageResource(R.drawable.ukflag)
-                binding.flagOne.contentDescription = "Ícone da bandeira do Reino Unido"
-            }
-
-            "CHF" -> {
-                controlInitialValue *= 5.91
-                binding.flagOne.setImageResource(R.drawable.chflag)
-                binding.flagOne.contentDescription = "Ícone da bandeira da Suíça"
-            }
-
-            "EUR" -> {
-                controlInitialValue *= 5.72
-                binding.flagOne.setImageResource(R.drawable.euflag)
-                binding.flagOne.contentDescription = "Ícone da bandeira da União Europeia"
-            }
-        }
-    }
-
-    private fun setupCurrencyTwoView() {
-        when (currencyTwo) {
-            "BRL" -> {
-                finalValue = 1.0
-                binding.flagTwo.setImageResource(R.drawable.brflag)
-                binding.flagTwo.contentDescription = "Ícone da Bandeira do Brasil"
-            }
-
-            "USD" -> {
-                finalValue = 5.3
-                binding.flagTwo.setImageResource(R.drawable.usflag)
-                binding.flagTwo.contentDescription = "Ícone da bandeira dos Estados Unidos"
-            }
-
-            "GBP" -> {
-                finalValue = 6.74
-                binding.flagTwo.setImageResource(R.drawable.ukflag)
-                binding.flagTwo.contentDescription = "Ícone da bandeira do Reino Unido"
-            }
-
-            "CHF" -> {
-                finalValue = 5.91
-                binding.flagTwo.setImageResource(R.drawable.chflag)
-                binding.flagTwo.contentDescription = "Ícone da bandeira da Suíça"
-            }
-
-            "EUR" -> {
-                finalValue = 5.72
-                binding.flagTwo.setImageResource(R.drawable.euflag)
-                binding.flagTwo.contentDescription = "Ícone da bandeira da União Europeia"
-            }
-        }
-    }
-
     private fun goToMainActivity() {
         startActivity(Intent(this, MainActivity::class.java).apply {
-            val bundle = Bundle().apply {
+            putExtra("bundle", Bundle().apply {
                 putStringArray("currenciesList", currenciesList)
-            }
-            putExtra("bundle", bundle)
+            })
         })
     }
 }
