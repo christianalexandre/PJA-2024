7e608dd632d68251144630e23023c7b804f0dce2
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-08-06 17:20:43 -0300

    refactor: use clean code in Main Activity

--- tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/MainActivity.kt
+++ tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.conversaomoedas
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -12,102 +11,106 @@ import com.example.conversaomoedas.objects.Currency
 import com.example.conversaomoedas.objects.TextWatcherHelper
 import com.example.listadecontatos.R
 import com.example.listadecontatos.databinding.MainActivityBinding
+import kotlin.properties.Delegates
 
 class MainActivity : ComponentActivity() {
     private lateinit var binding: MainActivityBinding
     private lateinit var bundle: Bundle
-    private lateinit var currenciesList: Array<String>
     private lateinit var spinnerOne: Spinner
     private lateinit var spinnerTwo: Spinner
+    private lateinit var spinnerOneSelectedItem: String
+    private lateinit var spinnerTwoSelectedItem: String
     private lateinit var initialValue: EditText
+    private var convertingValue by Delegates.notNull<Double>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initActivityVariables()
-        setupView()
+        getExtras()
+        setupSpinners()
         setupListeners()
 
         initialValue.setText("") // for some reason textWatcher doesn't work on first change in initialValue, so i put the first change here. now all changes will fall into textWatcher
-
-        getExtras()
+        setContentView(binding.root)
     }
 
     private fun initActivityVariables() {
         binding = MainActivityBinding.inflate(layoutInflater)
         spinnerOne = binding.currencyOne
         spinnerTwo = binding.currencyTwo
+        spinnerOneSelectedItem = "Selecionar uma moeda"
+        spinnerTwoSelectedItem = "Selecionar uma moeda"
         initialValue = binding.initialValue
-        setupSpinners()
-    }
-
-    private fun setupView() {
-        setContentView(binding.root)
     }
 
     private fun getExtras() {
-        bundle = intent.getBundleExtra("bundle") ?: return // if bundle == null returns the function
+        bundle = intent.getBundleExtra("bundle") ?: return
+        spinnerOneSelectedItem = bundle.getStringArray("currenciesList")!![0]
+        spinnerTwoSelectedItem = bundle.getStringArray("currenciesList")!![1]
+    }
 
-        val spinnerOneSelectedItem = bundle.getStringArray("currenciesList")!![0]
-        val spinnerTwoSelectedItem = bundle.getStringArray("currenciesList")!![1]
+    private fun setupSpinners() {
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.array_currencies,
+            R.layout.spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+            spinnerOne.adapter = adapter
+            spinnerTwo.adapter = adapter
+        }
 
         Currency.defineCurrencySelectedItem(spinnerOne, spinnerOneSelectedItem)
         Currency.defineCurrencySelectedItem(spinnerTwo, spinnerTwoSelectedItem)
     }
 
     private fun setupListeners() {
-        binding.calculateButton.setOnClickListener { goToConversionPage() }
+        binding.calculateButton.setOnClickListener {
+            spinnerOneSelectedItem = spinnerOne.selectedItem.toString()
+            spinnerTwoSelectedItem = spinnerTwo.selectedItem.toString()
+
+            if(!verifyIsBlank()) return@setOnClickListener
+            if(!verifyIsMissingSelectedCurrencies()) return@setOnClickListener
+            if(!verifyIsIdenticalSelectedCurrencies()) return@setOnClickListener
+
+            convertingValue = initialValue.text.toString().replace(',','.').toDouble()
+
+            goToConversionPage()
+        }
         initialValue.addTextChangedListener(TextWatcherHelper.filterChangedText(initialValue))
     }
 
-    private fun goToConversionPage() {
-        val spinnerOneSelectedItem = spinnerOne.selectedItem.toString()
-        val spinnerTwoSelectedItem = spinnerTwo.selectedItem.toString()
-        val convertingValue: Double
-        currenciesList = arrayOf(spinnerOneSelectedItem, spinnerTwoSelectedItem)
-
-        if (this.initialValue.text.toString().isBlank()) {
+    private fun verifyIsBlank(): Boolean {
+        if (initialValue.text.toString().isBlank()) {
             Toast.makeText(this, R.string.missing_convert_value, Toast.LENGTH_SHORT).show()
-            return
+            return false
         }
+        return true
+    }
 
-        try {
-            convertingValue = this.initialValue.text.toString().replace(',','.').toDouble()
-        } catch (_: NumberFormatException) {
-            Toast.makeText(this, R.string.valid_value, Toast.LENGTH_SHORT).show()
-            return
-        }
-
+    private fun verifyIsMissingSelectedCurrencies(): Boolean {
         if (spinnerOneSelectedItem == "Selecionar uma moeda" || spinnerTwoSelectedItem == "Selecionar uma moeda") {
             Toast.makeText(this, R.string.missing_selected_currencies, Toast.LENGTH_SHORT).show()
-        } else if (convertingValue <= 0) {
-            Toast.makeText(this, R.string.valid_value, Toast.LENGTH_SHORT).show()
-        } else if (spinnerOneSelectedItem == spinnerTwoSelectedItem) {
+            return false
+        }
+        return true
+    }
+
+    private fun verifyIsIdenticalSelectedCurrencies(): Boolean {
+        if (spinnerOneSelectedItem == spinnerTwoSelectedItem) {
             Toast.makeText(this, R.string.identical_currencies, Toast.LENGTH_SHORT).show()
-        } else {
-            setupExtras(convertingValue)
+            return false
         }
+        return true
     }
 
-    private fun setupExtras(convertingValue: Double) {
+    private fun goToConversionPage() {
         startActivity(Intent(this, ConversionPage::class.java).apply {
-            val bundle = Bundle().apply {
-                putStringArray("currenciesList", currenciesList)
+            putExtra("bundle", Bundle().apply {
+                putStringArray("currenciesList", arrayOf(spinnerOneSelectedItem, spinnerTwoSelectedItem))
                 putDouble("initialValue", convertingValue)
-            }
-            putExtra("bundle", bundle)
+            })
         })
-    }
-
-    @SuppressLint("ResourceType")
-    private fun setupSpinners() {
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.array_currencies,
-            R.drawable.spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(R.drawable.spinner_dropdown_item)
-            spinnerOne.adapter = adapter
-            spinnerTwo.adapter = adapter
-        }
     }
 }
