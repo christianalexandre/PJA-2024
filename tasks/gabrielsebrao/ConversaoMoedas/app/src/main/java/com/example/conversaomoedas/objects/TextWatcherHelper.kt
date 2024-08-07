148676f1f3fd6756fabeb390fe795d1941191375
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-08-07 17:08:51 -0300

    refactor: change function name

--- tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/objects/TextWatcherHelper.kt
+++ tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/objects/TextWatcherHelper.kt
@@ -6,7 +6,7 @@ import android.text.TextWatcher
 import android.widget.EditText
 
 object TextWatcherHelper {
-    fun filterChangedText(text: EditText): TextWatcher {
+    fun filterChangeTextForInitialValue(text: EditText): TextWatcher {
         return object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
@@ -14,8 +14,8 @@ object TextWatcherHelper {
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
                 val inputFilter = InputFilter { source, start, end, dest, dstart, dend ->
-                    val pattern = "^(\\d{0,8})([.,]?)$".toRegex()
-                    val decimalPattern = "^(\\d{0,8})([.,]?)(\\d{0,2})$".toRegex()
+                    val pattern = "^(\\d{0,9})([.,]?)$".toRegex()
+                    val decimalPattern = "^(\\d{0,9})([.,]?)(\\d{0,2})$".toRegex()
                     val noNumberPattern = "^([.,])$".toRegex()
                     val zeroPattern = "^(0)([^.,]*)".toRegex()
                     val result = dest.subSequence(0, dstart).toString() +
