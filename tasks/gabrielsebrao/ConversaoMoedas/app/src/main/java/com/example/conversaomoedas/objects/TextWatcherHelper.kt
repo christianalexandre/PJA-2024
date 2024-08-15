1d8fdc27b993bf260339cb1412f090c8191b05b2
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-08-15 17:23:20 -0300

    feat: put an edittext ability to convert user input into a currency format in real time

--- tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/objects/TextWatcherHelper.kt
+++ tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/objects/TextWatcherHelper.kt
@@ -4,47 +4,70 @@ import android.text.Editable
 import android.text.InputFilter
 import android.text.TextWatcher
 import android.widget.EditText
+import java.text.NumberFormat
 
 object TextWatcherHelper {
-    fun filterChangeTextForInitialValue(text: EditText): TextWatcher {
+    fun filterTextChangedForInitialValue(editText: EditText): TextWatcher {
         return object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
+            private var onlyOneCommaPattern = "^(,)$".toRegex()
+            private var doubleCommasPattern = "^(.*)(,,)$".toRegex()
+            private var anythingThatExceedsTwoNumbersAfterCommaPattern = "^([(\\d*).?]*)(,)(\\d{2}.)$".toRegex()
+            private val decimalNumberThatExceedsFourteenDigitsBeforeComma = "^([\\d.]{14})$".toRegex()
+
+            override fun beforeTextChanged(s: CharSequence?, startO: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(textChanged: CharSequence?, start: Int, before: Int, count: Int) {
+                editText.filters = arrayOf(InputFilter { source, start, end, dest, dstart, dend ->
+                        val result = dest.subSequence(0, dstart).toString() + source.subSequence(start, end) + dest.subSequence(dend, dest.length).toString()
+
+                        if (onlyOneCommaPattern.matches(result)) return@InputFilter ""
+                        if (doubleCommasPattern.matches(result)) return@InputFilter ""
+                        if (decimalNumberThatExceedsFourteenDigitsBeforeComma.matches(result)) return@InputFilter ""
+                        if (anythingThatExceedsTwoNumbersAfterCommaPattern.matches(result)) return@InputFilter ""
+
+                        editText.setSelection(editText.text.toString().length)
+                        null
+                    })
             }
 
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                val inputFilter = InputFilter { source, start, end, dest, dstart, dend ->
-                    val pattern = "^(\\d{0,9})([.,]?)$".toRegex()
-                    val decimalPattern = "^(\\d{0,9})([.,]?)(\\d{0,2})$".toRegex()
-                    val noNumberPattern = "^([.,])$".toRegex()
-                    val zeroPattern = "^(0)([^.,]*)".toRegex()
-                    val result = dest.subSequence(0, dstart).toString() +
-                            source.subSequence(start, end) +
-                            dest.subSequence(dend, dest.length).toString()
+            override fun afterTextChanged(textChanged: Editable?) {}
+        }
+    }
 
-                    if (noNumberPattern.matches(result)) {
-                        return@InputFilter ""
-                    }
+    fun formatTextChangedForInitialValue(editText: EditText): TextWatcher {
+        return object : TextWatcher {
 
-                    if (zeroPattern.matches(result) && result != "0") {
-                        return@InputFilter ""
-                    }
+            private var current = ""
+            private var decimalNumberWithOnlyACommaAndNothingThenPattern = "^([(\\d*).?]*)(,)$".toRegex()
+            private var decimalNumberWithOneZeroAfterCommaPattern = "^(.*)(,0)$".toRegex()
+            private var decimalNumberWithOneZeroAfterADecimalDigitPattern = "^(.*)(,\\d0)$".toRegex()
 
-                    if (('.' in result || ',' in result) && decimalPattern.matches(result)) {
-                        null
-                    } else if (pattern.matches(result)) {
-                        null
-                    } else {
-                        ""
-                    }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(textChanged: CharSequence?, start: Int, before: Int, count: Int) {
+
+                if (textChanged.toString() != current) {
+
+                    if (textChanged.toString().isEmpty()) return
+                    if (decimalNumberWithOnlyACommaAndNothingThenPattern.matches(textChanged.toString())) return
+                    if (decimalNumberWithOneZeroAfterCommaPattern.matches(textChanged.toString())) return
+                    if (decimalNumberWithOneZeroAfterADecimalDigitPattern.matches(textChanged.toString())) return
+
+                    val cleanString = textChanged.toString().replace("(\\.)".toRegex(), "").replace("(,)".toRegex(), ".")
+                    val parsed = cleanString.toDoubleOrNull() ?: 0.0
+                    val formatted = NumberFormat.getNumberInstance().format(parsed)
+
+                    editText.removeTextChangedListener(this)
+                    current = formatted
+                    editText.setText(formatted)
+                    editText.setSelection(formatted.length)
+
+                    editText.addTextChangedListener(this)
                 }
-
-                text.filters = arrayOf(inputFilter)
             }
 
-            override fun afterTextChanged(s: Editable?) {
-
-            }
+            override fun afterTextChanged(textChanged: Editable?) {}
         }
     }
 }
