f01beabf96b201e7fb63b5807ec26d4962cd43b3
JoaoVitor1203 <joaovitorcastilhos.s@gmail.com>
2024-08-20 14:05:46 -0300

    feat: lifeCycle debug

---
+++ tasks/JoaoVitor/app/src/main/java/com/example/pja_21_2/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.pja_21_2
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("debug", "onCreate: Acionado assim que o sistema cria a atividade")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("debug", "onStart: Quando a atividade entra no estado Iniciado")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("debug", "onResume: Quando a atividade entra no estado Retomado")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("debug", "onPause: Quando ocorre um evento de interrupção")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("debug", "onStop: Quando a atividade não está mais visível para o usuário")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("debug", "onDestroy: Quando a atividade é destruida")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("debug", "onRestart: Quando a atividade retorna do onStop() e retona ao onStart")
+    }
+}
