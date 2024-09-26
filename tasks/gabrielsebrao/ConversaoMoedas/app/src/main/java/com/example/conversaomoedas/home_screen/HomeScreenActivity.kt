23e9e746d538c82e78210cd0d7108ad9ad62cb89
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-09-26 16:59:54 -0300

    refactor: remove unnecessary import

--- tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/home_screen/HomeScreenActivity.kt
+++ tasks/gabrielsebrao/ConversaoMoedas/app/src/main/java/com/example/conversaomoedas/home_screen/HomeScreenActivity.kt
@@ -3,7 +3,6 @@ package com.example.conversaomoedas.home_screen
 import android.content.Intent
 import android.graphics.Rect
 import android.os.Bundle
-import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.widget.AdapterView
